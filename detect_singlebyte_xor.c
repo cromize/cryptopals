05a2d9dd16fd95058e3e793154ef127e55cacedc
cromize <mikezemanek@gmail.com>
2018-06-12 23:24:55 +0200

    refactor

--- detect_singlebyte_xor.c
+++ detect_singlebyte_xor.c
@@ -5,28 +5,30 @@
 #include <string.h>
 #include <stdlib.h>
 #include "helpers.h"
+#include "detect_singlebyte_xor.h"
 #include "singlebyte_xor_decrypt.h"
 
-void detect_xor_cipher(const char* input_file, char* output) {
+void detect_singlebyte_xor_cipher(const char* input_file, char* output) {
   FILE *input;
   int score_arr[DEFAULT_SIZE] = {0};
   char buf[256];  
-  char input_arr[DEFAULT_SIZE][256] = {0};
+  char xored_arr[DEFAULT_SIZE][256] = {0};
 
   input = fopen(input_file, "r");
   if (input == 0) 
     return -1;
 
+  // process each line in file and save it into memory
   int i = 0;
   while (fgets(buf, 256, input) != 0) {
     char temp[256];
     int score;
     crack_singlebyte_xor(buf, temp, &score);
-    //printf("%d. score: %d string: %s\n", i++, highest_score, temp);
-    strcpy(input_arr[i], temp);
+    strcpy(xored_arr[i], temp);
     score_arr[i++] = score;
   }
 
+  // find line with highest score
   int highest = 0;
   int pos = 0;
   for (int i = 0; i < DEFAULT_SIZE; i++) {
@@ -36,7 +38,8 @@ void detect_xor_cipher(const char* input_file, char* output) {
     }
   }
 
-  printf("%d. score: %d string: %s\n", pos, highest, input_arr[pos]);
+  strcpy(output, xored_arr[pos]);
+  return;
 }
 
 #ifndef DETECT_SINGLEBYTE_XOR 
@@ -50,9 +53,10 @@ int main(int argc, char* argv[]) {
     exit(0);
   }
 
-  // detect
+  // detect 
   if (argc == 2) {
-    detect_xor_cipher(argv[1], 0);    
+    detect_singlebyte_xor_cipher(argv[1], output);    
+    printf("string: %s", output);
   } 
 
   return 0;
---
+++ detect_singlebyte_xor.h
@@ -0,0 +1,6 @@
+#ifndef DETECT_SINGLEBYTE_XOR_H
+#define DETECT_SINGLEBYTE_XOR_H
+
+void detect_singlebyte_xor_cipher(const char* input_file, char* output); 
+
+#endif
