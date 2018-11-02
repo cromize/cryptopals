0d610762b2fc485d60c405993586adcf7eb39fc0
cromize <mikezemanek@gmail.com>
2018-11-02 02:14:54 +0100

    refactor

--- python/challenges/1.py
+++ python/challenges/1.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import binascii
-import base64
-
-def b_to_hex(input):
-  return base64.b64encode(input)
-
-def hex_to_b(input):
-  return binascii.hexlify(base64.b64decode(input))
+from base64 import b64encode, b64decode
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
+  if len(sys.argv) <= 1:
     print(f'{sys.argv[0]}: [-d] input')
     sys.exit(0)
 
-  if len(sys.argv) < 3:
-    print(str(b_to_hex(binascii.unhexlify(sys.argv[1])), 'ascii'))
+  if len(sys.argv) == 2:
+    i1 = binascii.unhexlify(sys.argv[1])
+    print(str(b64encode(i1), 'ascii'))
 
-  if len(sys.argv) < 4:
+  if len(sys.argv) == 3:
     if sys.argv[1] == '-d':
-      print(str(hex_to_b(sys.argv[2]), 'ascii'))
+      o1 = b64decode(sys.argv[2])
+      print(str(binascii.hexlify(o1), 'ascii'))
      
