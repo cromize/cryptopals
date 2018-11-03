86d0d24c1413284f4c8c0f9d9dd4c7e49c7ba299
cromize <mikezemanek@gmail.com>
2018-11-03 22:51:46 +0100

    python 3. fix scoring system

--- python/xor.py
+++ python/xor.py
@@ -3,10 +3,12 @@ def score_string(string):
   score_table = "etaoinshrdlcu mwfgypbvkjxqz"[::-1]
   score = 0
   for idx, k in enumerate(score_table):
-    if k in string.lower():
+    if k in string:  
       score += idx 
+    elif k.upper() in string:
+      score += idx//2 
     else:
-      score -= 5
+      score -= 1
   return score
 
 def xor_key(string, key):
@@ -18,11 +20,10 @@ def xor_key(string, key):
 def crack_cipher(cipher):
   best_score = 0
   best_str = ""
-  xored_str = ""
   for key in range(0, 128):
     xored_str = xor_key(cipher, key)
     score = score_string(xored_str)
     if score > best_score:
       best_score = score  
       best_str = xored_str
-  return best_str
+  return best_str, best_score
