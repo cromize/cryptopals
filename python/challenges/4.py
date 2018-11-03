1db8b1317107bffd126a0e2a31e0ecbf335f69ab
cromize <mikezemanek@gmail.com>
2018-11-03 22:52:21 +0100

    4. challenge in python works

---
+++ python/challenges/4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import binascii
+sys.path.append('..')
+from helpers import abort
+from xor import crack_cipher
+
+def detect_xored_cipher(filename):
+  with open(sys.argv[1]) as cipher_file:
+    best_score = 0
+    best_line = ""
+    for line in cipher_file:
+      plaintext, score = crack_cipher(binascii.unhexlify(line.rstrip()))
+      if score > best_score:
+        best_score = score
+        best_line = plaintext 
+  return best_line.rstrip()
+
+if __name__ == "__main__":
+  if len(sys.argv) == 2:
+    print(detect_xored_cipher(sys.argv[1]))
+  else: 
+    abort(f'{sys.argv[0]}: filename')
+
