6f6264a271f197811aae62f396cf05e069ab9cac
cromize <mikezemanek@gmail.com>
2018-11-03 20:41:32 +0100

    move xor crack functions to separate folder

--- python/challenges/3.py
+++ python/challenges/3.py
@@ -3,34 +3,7 @@ import sys
 import binascii
 sys.path.append('..')
 from helpers import abort, xor
-
-def score_string(string):
-  score_table = "etaoinshrdlcu mwfgypbvkjxqz"[::-1]
-  score = 0
-  for idx, k in enumerate(score_table):
-    if k in string.lower():
-      score += idx 
-    else:
-      score -= 5
-  return score
-
-def xor_key(string, key):
-  xored_str = "" 
-  for ch in string:
-    xored_str += chr(ch ^ key)
-  return xored_str
-
-def crack_cipher(cipher):
-  best_score = 0
-  best_str = ""
-  xored_str = ""
-  for key in range(0, 128):
-    xored_str = xor_key(cipher, key)
-    score = score_string(xored_str)
-    if score > best_score:
-      best_score = score  
-      best_str = xored_str
-  return best_str
+from xor import crack_cipher
 
 if __name__ == "__main__":
   if len(sys.argv) == 2:
