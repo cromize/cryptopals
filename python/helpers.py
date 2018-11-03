58ba29cd0a93a5fa83a511dd208b92fa5c3dd9f2
cromize <mikezemanek@gmail.com>
2018-11-04 00:26:40 +0100

    add python hamming distance function

--- python/helpers.py
+++ python/helpers.py
@@ -15,3 +15,15 @@ def xor(s1, s2):
     output += bytes([s1[i] ^ s2[i]])
 
   return output
+
+def hamming_dst(str1, str2):
+  if len(str1) != len(str2):
+    raise ValueError("string lengths doesn't match")
+  bcount = 0
+  for i in range(len(str1)): 
+    val = str1[i] ^ str2[i]
+    while val:
+      bcount += 1
+      # remove 1 bit each time
+      val &= val-1
+  return bcount
