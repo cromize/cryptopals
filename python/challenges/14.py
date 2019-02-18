e488fa332258de139b6155c9cbcec61d011263b7
cromize <mikezemanek@gmail.com>
2019-02-18 21:24:49 +0100

    python: 14. successfuly aligned offset in block, input string into oracle is too long though. let's write a better approach.

--- python/challenges/14.py
+++ python/challenges/14.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import random
+import string
 sys.path.append('..')
 from block_crypto import aes_ecb_encrypt, aes_ecb_decrypt, get_random_bytes, aes_ecb_detect
 from helpers import abort, gen_identical_bytes
@@ -15,7 +16,7 @@ key = get_random_bytes(16)
 
 def oracle(plaintext, append, key):
   random_prefix = get_random_bytes(random.randrange(1, 48))
-  to_cipher = random_prefix + plaintext + append
+  to_cipher = random_prefix + plaintext + b64decode(append)
   cipher = aes_ecb_encrypt(to_cipher, key)
   return cipher
 
@@ -64,59 +65,6 @@ def match_dictionary(cipher, dictionary):
     if block == v:
       return k.decode()
 
-def make_sentinel_dictionary(template):
-  dictionary = dict()
-  for x in range(16):
-    tmp = 4*16*b"b" + x*b"b" + template
-    cipher = oracle(tmp, b64decode(to_append), key)
-    sentinel_idx = find_sentinel(cipher)
-    #print(aes_ecb_decrypt(cipher[16*(sentinel_idx+1):16*(sentinel_idx+2)], key))
-    dictionary[tmp[0:16]] = cipher[16*(sentinel_idx+1):16*(sentinel_idx+2)]
-  return dictionary
-"""
-  for x in range(16):
-    tmp = template + x*b"b"
-    cipher = oracle(tmp, b64decode(to_append), key)
-    sentinel_idx = find_sentinel(cipher)
-    # idx is last 16 bytes
-    dictionary[tmp[len(tmp)-16:len(tmp)]] = cipher[16*(sentinel_idx+1):16*(1+sentinel_idx+1)]
-"""
-
-"""
-# we need to make a function to forge blocks
-# 4*prepend + sentinel + 4*append
-def find_forge(ciphertext, offset):
-  cipher_blocks = [ciphertext[i+offset:i+offset+16] for i in range(0, len(ciphertext), 16)]
-  counter = Counter(cipher_blocks)
-  forge_block = counter.most_common()[0][0]
-  print(counter.most_common())
-  last_sentinel_idx = 0
-  for idx, block in enumerate(cipher_blocks):
-    if block == forge_block:
-      print("FOUND")
-      print(block)
-      last_sentinel_idx = idx
-  return last_sentinel_idx
-
-def make_forge_dict():
-  unique_sled = b"0123456789abcdef"
-  dictionary = dict()
-  for x in range(16):
-    tmp = 4*16*b"b" + x*b"b" + unique_sled + 4*16*b"c"
-    cipher = oracle(tmp, b64decode(to_append), key)
-    forge_a_idx = find_forge(cipher, 0)
-    print("forge_a:", forge_a_idx)
-
-    forge_b_idx = find_forge(cipher, 16*(forge_a_idx-1))
-    print("forge_b:", forge_b_idx)
-
-    diff = forge_b_idx - forge_a_idx
-    #print(aes_ecb_decrypt(cipher[16*(diff+1):16*(diff+2)], key))
-    sys.exit(0)
-    dictionary[tmp[0:16]] = cipher[16*(sentinel_idx+1):16*(sentinel_idx+2)]
-  return dictionary
-"""
-
 # find the most common block, get idx and dup. count from that
 def find_sentinel(ciphertext):
   cipher_blocks = [ciphertext[i:i+16] for i in range(0, len(ciphertext), 16)]
@@ -130,31 +78,44 @@ def find_sentinel(ciphertext):
       last_sentinel_idx = idx
   
   occurrence = [counter.most_common()[idx][1] for idx in range(len(counter.most_common()))]
+  idx_start = 0
   for idx, i in enumerate(occurrence):
+    dup_cnt = counter.most_common()[idx][1]
+    if idx_start == 0 and idx != 1:
+      idx_start = idx
     if (i % 2.0) == 0:
-      return last_sentinel_idx, counter.most_common()[idx][1]
-  return None, None
-      
+      if dup_cnt >= 32:
+        return last_sentinel_idx, dup_cnt, idx - idx_start + 1
+      else:
+        return last_sentinel_idx, dup_cnt, idx - idx_start + 3
+  return None, None, None
+
+def make_offset_sentinel_pattern():
+  pattern = b""
+  for idx, i in enumerate(range(4, 36, 2)):
+    pattern += 16 * i * string.ascii_lowercase[idx].encode() + b"Z"
+  return pattern
+  
 if __name__ == "__main__":
   if len(sys.argv) == 1:
-    sentinel_sled = 4*16*b"a" + b"z" + 6*16*b"b" + b"z" + 8*16*b"c" + b"z" + 10*16*b"d" + b"z"
-    success_times = 1
-    fail_times = 0
+    #sentinel_sled = 4*16*b"a" + b"z" + 6*16*b"b" + b"z" + 8*16*b"c" + b"z" + 10*16*b"d" + b"z"
+    sentinel_sled = make_offset_sentinel_pattern()
+    #print(len(sentinel_sled))
+    #sys.exit(0)
+    success_times = 0
+    i = 0
     while 1:
       cipher = oracle(sentinel_sled, to_append, key)
-      sentinel_idx, dup_cnt = find_sentinel(cipher)
-      if dup_cnt == 4 or dup_cnt == 6 or dup_cnt == 8:
-        print(f"{success_times}/{fail_times}", "sucess/miss,", f"ratio {(success_times/fail_times)*100}%")
+      sentinel_idx, dup_cnt, idx_start = find_sentinel(cipher)
+      i += 1
+      if dup_cnt != None and (dup_cnt % 2) == 0:
+        print("aligned sentinel block idx:", sentinel_idx)
+        print("align offset in block:", idx_start)
+        print(aes_ecb_decrypt(cipher[16*(sentinel_idx+1):16*(sentinel_idx+2)], key)[idx_start:])
+        sys.exit(0)
         success_times += 1
-      else:
-        fail_times += 1
+        print(f"{success_times}/{i}", "sucess/all,", f"ratio {(success_times/i)*100:.2f}%")
 
-    sys.exit(0)
-    unique_sled = b"0123456789abcdef"
-    sentinel_dict = make_sentinel_dictionary(unique_sled)
-    #print(match_dictionary(cipher, sentinel_dict))
-
-    #make_forge_dict()
     sys.exit(0)
 
     #TODO: lets crack tha blooocks
